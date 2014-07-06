f56f5e701bc85b6413f8769d06139f261fea56a9
Javier Garrido <javierationmex@gmail.com>
2014-07-05 20:16:20 -0700

    Added ability to write the current crimes to storage

--- app/src/main/java/com/javiergarrido/android/criminalintent/Crime.java
+++ app/src/main/java/com/javiergarrido/android/criminalintent/Crime.java
@@ -1,5 +1,8 @@
 package com.javiergarrido.android.criminalintent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,6 +10,11 @@ import java.util.UUID;
  * Created by Javier G on 6/14/2014.
  */
 public class Crime {
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
 
     private UUID mId;
     private String mTitle;
@@ -17,6 +25,15 @@ public class Crime {
         //Generate unique identifier
         mId = UUID.randomUUID();
         mDate = new Date();
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_SOLVED, mSolved);
+        json.put(JSON_DATE, mDate.getTime());
+        return json;
     }
 
     @Override
