9fa1df3a0e87149d74417747c8ae376db01ba307
Javier Garrido <javierationmex@gmail.com>
2014-07-17 16:31:01 -0700

    Added code to get camera and release camera at the appropriate times. Also coded to display the preview on the surface when the surface has been created and release it when the surface is destroyed.

--- app/src/main/java/com/javiergarrido/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/javiergarrido/android/criminalintent/CrimeCameraFragment.java
@@ -1,13 +1,21 @@
 package com.javiergarrido.android.criminalintent;
 
-import android.graphics.Camera;
+import android.annotation.TargetApi;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Javier G on 7/17/2014.
@@ -19,6 +27,7 @@ public class CrimeCameraFragment extends Fragment {
     private SurfaceView mSurfaceView;
 
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragement_crime_camera, parent, false);
@@ -33,10 +42,70 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        final SurfaceHolder holder = mSurfaceView.getHolder();
+        // setType() and SURFACE_TYPE_PUSH_BUFFERS are both deprecated,
+        // but are required for Camera preview to work on pre-3.0 devices.
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                // Tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null){
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int w, int h) {
+                if (mCamera != null) return;
+
+                // The surface has changed size; update the camera preview size
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                // We can no longer display on this surface, so stop the preview.
+                if (mCamera != null){
+                    mCamera.stopPreview();
+                }
+            }
+        });
 
         return v;
     }
 
+    /*** A simple algorithm to get the largest size3 available. For a more
+     *  robust version, see CameraPreview.java inn the ApiDemos
+     *  sample app from Android. */
+
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, int width, int height){
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Camera.Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea){
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+    }
     public class CrimeCameraActivity extends SingleFragmentActivity{
         @Override
         protected Fragment createFragment() {
@@ -44,5 +113,25 @@ public class CrimeCameraFragment extends Fragment {
         }
     }
 
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD){
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 }
 
